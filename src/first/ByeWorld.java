65df7fad90fb2f29b2a7ca26163fe80c0267c228
Nonna <mamedova0720@gmail.com>
2022-09-21 01:39:02 -0400

    Added ByeWorld.java

---
+++ src/first/ByeWorld.java
@@ -0,0 +1,9 @@
+package first;
+
+public class ByeWorld {
+    public static void main(String[] args) {
+        System.out.println("Java is almost over");
+        System.out.println("Bye World");
+
+    }
+}
